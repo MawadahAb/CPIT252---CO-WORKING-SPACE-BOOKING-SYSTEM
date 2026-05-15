076c543a1fb7a94717c6791a041a2edb80f177c2
salwa-xix <salwaabdulrahim858@gmail.com>
2026-05-15 08:15:27 +0000

    WhiteboardDecorator class added

---
+++ src/main/java/decorator/WhiteboardDecorator.java
@@ -0,0 +1,18 @@
+
+
+public class WhiteboardDecorator extends WorkspaceDecorator {
+
+    public WhiteboardDecorator(Workspace workspace) {
+        super(workspace);
+    }
+
+    @Override
+    public double getCost() {
+        return workspace.getCost() + 5;
+    }
+
+    @Override
+    public String getDescription() {
+        return workspace.getDescription() + " + Whiteboard";
+    }
+}
