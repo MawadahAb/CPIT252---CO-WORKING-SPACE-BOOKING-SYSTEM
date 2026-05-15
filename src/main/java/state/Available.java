5d89cd490c9191a7acc7183ad1a17e6617115d51
mawadah <mawalahmadi@gmail.com>
2026-05-16 01:34:43 +0300

    Available class added

---
+++ src/main/java/state/Available.java
@@ -0,0 +1,40 @@
+package state;
+
+import paymentStrategy.PaymentContext;
+import workspace.Workspace;
+
+public class Available implements BookingState {
+
+    @Override
+    public void book(BookingContext context) {
+        boolean reserved = context.getInventory().reserve(context.getWorkspace());
+
+        if (reserved) {
+            System.out.println("Workspace has been reserved.");
+            context.changeState(new Reserved());
+        } else {
+            System.out.println("Workspace is not available.");
+        }
+    }
+
+    @Override
+    public void addService(BookingContext context, Workspace newWorkspace, String serviceName) {
+        System.out.println("Cannot add " + serviceName + " before booking.");
+    }
+
+    @Override
+    public void checkIn(BookingContext context) {
+        System.out.println("Cannot check in before booking.");
+    }
+
+    @Override
+    public void checkOut(BookingContext context, PaymentContext paymentContext) {
+        System.out.println("Cannot check out before booking.");
+    }
+
+    @Override
+    public void cancel(BookingContext context) {
+        System.out.println("No booking to cancel.");
+    }
+
+}
