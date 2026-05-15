31c1c5b5c49ac4024fa19a9e5d142ec113c751be
Razan Aljezani <raljezani1@gmail.com>
2026-05-15 20:37:42 +0300

    interface PaymentStrategy added

---
+++ src/main/java/paymentStrategy/PaymentReceipt.java
@@ -0,0 +1,4 @@
+package paymentStrategy;
+
+public class PaymentReceipt {
+}
---
+++ src/main/java/paymentStrategy/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package paymentStrategy;
+
+public interface PaymentStrategy {
+    PaymentReceipt pay (double amount);
+}
