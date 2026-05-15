a55dd8f5bbb7710c00555e6e348cd081cd61e682
mawadah <mawalahmadi@gmail.com>
2026-05-16 01:44:01 +0300

    CheckedOut class added

---
+++ src/main/java/state/CheckedOut.java
@@ -0,0 +1,32 @@
+package state;
+
+import paymentStrategy.PaymentContext;
+import workspace.Workspace;
+
+public class CheckedOut implements BookingState {
+
+    @Override
+    public void book(BookingContext context) {
+        System.out.println("Booking is already completed.");
+    }
+
+    @Override
+    public void addService(BookingContext context, Workspace newWorkspace, String serviceName) {
+        System.out.println("Cannot add " + serviceName + " after check out.");
+    }
+
+    @Override
+    public void checkIn(BookingContext context) {
+        System.out.println("Cannot check in after check out.");
+    }
+
+    @Override
+    public void checkOut(BookingContext context, PaymentContext paymentContext) {
+        System.out.println("Customer already checked out.");
+    }
+
+    @Override
+    public void cancel(BookingContext context) {
+        System.out.println("Cannot cancel completed booking.");
+    }
+}
