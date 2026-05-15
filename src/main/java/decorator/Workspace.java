fa6621823fb55dfb964a2a957bf60be543c28125
salwa-xix <salwaabdulrahim858@gmail.com>
2026-05-15 08:01:11 +0000

    interfaceWorkspace and workspaceDecorator class added

---
+++ src/main/java/decorator/Workspace.java
@@ -0,0 +1,10 @@
+
+public 
+ Workspace {
+
+    int getCapacity();
+
+    double getCost();
+
+    String getDescription();
+}
---
+++ src/main/java/decorator/WorkspaceDecorator.java
@@ -0,0 +1,26 @@
+
+import workspace.core.Workspace;
+
+public abstract class WorkspaceDecorator implements Workspace {
+
+    protected Workspace workspace;
+
+    public WorkspaceDecorator(Workspace workspace) {
+        this.workspace = workspace;
+    }
+
+    @Override
+    public int getCapacity() {
+        return workspace.getCapacity();
+    }
+
+    @Override
+    public double getCost() {
+        return workspace.getCost();
+    }
+
+    @Override
+    public String getDescription() {
+        return workspace.getDescription();
+    }
+}
