f9c280ed431768ec2dbb41d1bd71902c256671f8
Razan Aljezani <raljezani1@gmail.com>
2026-05-17 02:42:36 +0300

    Added Documantion to CancelledState class

--- src/main/java/state/CancelledState.java
+++ src/main/java/state/CancelledState.java
@@ -5,26 +5,31 @@ import workspace.Workspace;
 
 public class CancelledState implements BookingState {
 
+    // Prevent booking after cancellation
     @Override
     public void book(BookingContext context) {
         System.out.println("Cannot book because this booking is cancelled.");
     }
 
+    // Prevent adding services to cancelled booking
     @Override
     public void addService(BookingContext context, Workspace newWorkspace, String serviceName) {
         System.out.println("Cannot add " + serviceName + " to cancelled booking.");
     }
 
+    // Prevent check in after cancellation
     @Override
     public void checkIn(BookingContext context) {
         System.out.println("Cannot check in to cancelled booking.");
     }
 
+    // Prevent check out after cancellation
     @Override
     public void checkOut(BookingContext context, PaymentContext paymentContext) {
         System.out.println("Cannot check out cancelled booking.");
     }
 
+    // Booking already cancelled
     @Override
     public void cancel(BookingContext context) {
         System.out.println("Booking is already cancelled.");
