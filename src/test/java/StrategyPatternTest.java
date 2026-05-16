00abc26e9934dd820f12d7c801554827b08f0b32
Razan Aljezani <raljezani1@gmail.com>
2026-05-17 02:14:18 +0300

    fix erroe in testing method in StrategyPatternTest

--- src/test/java/StrategyPatternTest.java
+++ src/test/java/StrategyPatternTest.java
@@ -12,120 +12,64 @@ public class StrategyPatternTest {
     @Test
     void shouldUseCreditCardPaymentStrategySuccessfully() {
 
-        PaymentContext context =
-                new PaymentContext(
-                        new CreditCard(
-                                "Salwa",
-                                "123456789",
-                                "123",
-                                "12/27"
-                        )
-                );
+        PaymentContext context = new PaymentContext(new CreditCard("Salwa", "123456789", "123", "12/27"));
 
-        PaymentReceipt receipt =
-                context.executePayment(200);
+        PaymentReceipt receipt = context.executePayment(200);
 
         assertTrue(receipt.isSuccessful());
 
         assertEquals(200, receipt.getAmount());
 
-        assertEquals(
-                "Credit Card",
-                receipt.getPaymentMethod()
-        );
+        assertEquals("Credit Card", receipt.getPaymentMethod());
     }
 
     @Test
     void shouldUsePayPalPaymentStrategySuccessfully() {
 
-        PaymentContext context =
-                new PaymentContext(
-                        new PayPal(
-                                "salwa@gmail.com",
-                                "12345"
-                        )
-                );
+        PaymentContext context = new PaymentContext(new PayPal("salwa@gmail.com", "12345"));
 
-        PaymentReceipt receipt =
-                context.executePayment(150);
+        PaymentReceipt receipt = context.executePayment(150);
 
         assertTrue(receipt.isSuccessful());
 
         assertEquals(150, receipt.getAmount());
 
-        assertEquals(
-                "PayPal",
-                receipt.getPaymentMethod()
-        );
+        assertEquals("PayPal", receipt.getPaymentMethod());
     }
 
     @Test
     void shouldRejectPaymentWhenAmountIsZero() {
 
-        PaymentContext context =
-                new PaymentContext(
-                        new CreditCard(
-                                "Salwa",
-                                "123456789",
-                                "123",
-                                "12/27"
-                        )
-                );
+        PaymentContext context = new PaymentContext(new CreditCard("Salwa", "123456789", "123", "12/27"));
 
-        PaymentReceipt receipt =
-                context.executePayment(0);
+        PaymentReceipt receipt = context.executePayment(0);
 
         assertFalse(receipt.isSuccessful());
 
-        assertEquals(
-                "Invalid amount",
-                receipt.getMessage()
-        );
+        assertEquals("Invalid amount", receipt.getMessage());
     }
 
     @Test
     void shouldRejectCreditCardPaymentWhenCardNumberIsInvalid() {
 
-        PaymentContext context =
-                new PaymentContext(
-                        new CreditCard(
-                                "Salwa",
-                                "123",
-                                "123",
-                                "12/27"
-                        )
-                );
+        PaymentContext context = new PaymentContext(new CreditCard("Salwa", "123", "123", "12/27"));
 
-        PaymentReceipt receipt =
-                context.executePayment(200);
+        PaymentReceipt receipt = context.executePayment(200);
 
         assertFalse(receipt.isSuccessful());
 
-        assertEquals(
-                "Invalid card number",
-                receipt.getMessage()
-        );
+        assertEquals("Invalid card number", receipt.getMessage());
     }
 
     @Test
     void shouldRejectPayPalPaymentWhenEmailIsInvalid() {
 
-        PaymentContext context =
-                new PaymentContext(
-                        new PayPal(
-                                "salwagmail.com",
-                                "12345"
-                        )
-                );
+        PaymentContext context = new PaymentContext(new PayPal("salwagmail.com", "12345"));
 
-        PaymentReceipt receipt =
-                context.executePayment(100);
+        PaymentReceipt receipt = context.executePayment(100);
 
         assertFalse(receipt.isSuccessful());
 
-        assertEquals(
-                "Invalid email",
-                receipt.getMessage()
-        );
+        assertEquals("Invalid email", receipt.getMessage());
     }
 }
